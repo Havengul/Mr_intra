0d1a9d6b77ea3dd07c1801154cc45365e415d4a5
Havengul <havengulrunebinder@gmail.com>
2017-03-02 08:43:25 +0200

    whateber brah this should work

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
-  get 'pages/test'
-  get 'pages/shower' => 'starter#shower'
-  #root 'starter#shower'
-  resources :pages do
-    collection do
-      get :shower
-    end
-  end
+  get 'pages/shower' => 'pages#shower'
+  get 'pages/allusers' => 'pages#allusers'
+#  resources :pages do
+#    collection do
+#      get :shower
+#    end
+#  end
 end
