d7b3d3d753cab323932a0989f02fa6136db35eb4
Havengul <havengulrunebinder@gmail.com>
2017-03-03 06:36:43 +0200

    got some junk files to remove

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'pages/shower' => 'pages#shower'
   get 'pages/allusers' => 'pages#allusers'
+  get 'pages/refreshusers' => 'pages#refreshusers'
 #  resources :pages do
 #    collection do
 #      get :shower
